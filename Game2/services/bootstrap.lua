7fa6ad44d7507a1c466699934a8789e3258239c1
Uac999 <vthanhp2011@gmail.com>
2026-03-24 13:34:23 +0700

    x

--- Game2/services/bootstrap.lua
+++ Game2/services/bootstrap.lua
@@ -1,8 +1,11 @@
 local skynet = require "skynet"
 
 skynet.start(function()
+	-- Bắt buộc phải khởi tạo launcher TRƯỚC bằng cách low-level này
+    local launcher = assert(skynet.launch("snlua", "launcher"))
+    skynet.name(".launcher", launcher)
     -- Khởi tạo launcher ĐẦU TIÊN (bắt buộc)
-    skynet.newservice("launcher")
+    --skynet.newservice("launcher")
 
     -- Sau đó mới khởi tạo main service của game
     skynet.newservice("main")          -- hoặc "gamed" tùy source của bạn
