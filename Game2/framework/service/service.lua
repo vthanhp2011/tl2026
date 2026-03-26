147536c6fce074b26c7b6ad3fceeb604fe3830ec
Uac999 <vthanhp2011@gmail.com>
2026-03-26 14:44:38 +0700

    Create service.lua

---
+++ Game2/framework/service/service.lua
@@ -0,0 +1,21 @@
+local skynet = require "skynet"
+local service = require "skynet.service"
+
+skynet.start(function()
+    skynet.register(".service")
+    skynet.error("=== .service REGISTERED SUCCESSFULLY ===")
+
+    skynet.dispatch("lua", function(session, source, command, ...)
+        if command == "LAUNCH" then
+            local ok, addr = pcall(service.new, ...)
+            if ok then
+                skynet.ret(skynet.pack(addr))
+            else
+                skynet.ret()
+                skynet.error("LAUNCH service failed:", ...)
+            end
+        elseif command == "KILL" then
+            skynet.kill(...)
+        end
+    end)
+end)
